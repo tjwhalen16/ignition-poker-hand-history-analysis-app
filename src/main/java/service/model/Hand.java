617c03a0d3b432a8f1e805c71b95299e21a81b96
Tyler Whalen <tjwhalen16@gmail.com>
2018-05-11 16:14:39 -0700

    Changed Hand from interface to abstract class to make it easier to reuse in upcoming addition

--- src/main/java/service/model/Hand.java
+++ src/main/java/service/model/Hand.java
@@ -8,15 +8,50 @@ import service.model.impl.Open;
 import service.model.impl.Player;
 import service.model.impl.PositionEnum;
 
-public interface Hand {
-	public Player getPlayerWithPosition(PositionEnum position);
-	public int getNumberPlayers();
+public abstract class Hand {
+	private List<Player> players;
+	private Blinds blinds;
+	private Timestamp time;
+		
+	/**
+	 * Returns the player in this hand with the position
+	 * @param position - position that desired player has
+	 * @return Player with PositionEnum position
+	 */
+	public Player getPlayerWithPosition(PositionEnum position) {
+		for (Player player : players) {
+			if (player.getPosition().equals(position)) {
+				return player;
+			}
+		}
+		
+		throw new IllegalArgumentException("No player with position: " + position);
+	}
 	
-	public List<Player> getPlayers();
-	public void setPlayers(List<Player> players);
-	public Blinds getBlinds();
-	public void setBlinds(Blinds blinds);
-	public Timestamp getTime();
-	public void setTime(Timestamp time);
-	
+	/**
+	 * Gets the number of players dealt cards in the hand
+	 * @return int - the number of players in the hand
+	 */
+	public int getNumberPlayers() {
+		return players.size();
+	}
+		
+	public List<Player> getPlayers() {
+		return players;
+	}
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+	public Blinds getBlinds() {
+		return blinds;
+	}
+	public void setBlinds(Blinds blinds) {
+		this.blinds = blinds;
+	}
+	public Timestamp getTime() {
+		return time;
+	}
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}		
 }
--- src/main/java/service/model/impl/CashGameHand.java
+++ src/main/java/service/model/impl/CashGameHand.java
@@ -1,79 +1,27 @@
 package service.model.impl;
 
-import java.sql.Timestamp;
-import java.util.List;
-
-import service.model.Blinds;
 import service.model.Hand;
 
 /**
- * One hand in a cash game
+ * One hand in a cash game with an open
  * @author Tyler
  *
  */
-public class CashGameHand implements Hand {
-
-	private List<Player> players;
+public class CashGameHand extends Hand {
 	private Open open;
-	private Blinds blinds;
 	private Player winner;
-	private Timestamp time;
 	
-	
-	/**
-	 * Returns the player in this hand with the position
-	 * @param position - position that desired player has
-	 * @return Player with PositionEnum position
-	 */
-	public Player getPlayerWithPosition(PositionEnum position) {
-		for (Player player : players) {
-			if (player.getPosition().equals(position)) {
-				return player;
-			}
-		}
-		
-		throw new IllegalArgumentException("No player with position" + position);
-	}
-	
-	/**
-	 * Gets the number of players dealt cards in the hand
-	 * @return int - the number of players in the hand
-	 */
-	public int getNumberPlayers() {
-		return players.size();
-	}
-	
-	
-	public List<Player> getPlayers() {
-		return players;
-	}
-	public void setPlayers(List<Player> players) {
-		this.players = players;
-	}
 	public Open getOpen() {
 		return open;
 	}
 	public void setOpen(Open open) {
 		this.open = open;
 	}
-	public Blinds getBlinds() {
-		return blinds;
-	}
-	public void setBlinds(Blinds blinds) {
-		this.blinds = blinds;
-	}
+
 	public Player getWinner() {
 		return winner;
 	}
 	public void setWinner(Player winner) {
 		this.winner = winner;
-	}
-	public Timestamp getTime() {
-		return time;
-	}
-	public void setTime(Timestamp time) {
-		this.time = time;
-	}
-	
-	
+	}	
 }
