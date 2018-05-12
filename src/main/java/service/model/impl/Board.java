c40c000db0b9280466adc31501efab56133d9bf7
Tyler Whalen <tjwhalen16@gmail.com>
2018-05-11 20:26:23 -0700

    Added default constructor and method for adding one card at a time to Board class. Also Got rid of brackets on toString output

--- src/main/java/service/model/impl/Board.java
+++ src/main/java/service/model/impl/Board.java
@@ -2,9 +2,14 @@ package service.model.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Board {
 	private List<Card> cards;
+	
+	public Board() {
+		this.cards = new ArrayList<>();
+	}
 	
 	public Board(String[] cards) {
 		this.cards = new ArrayList<>();
@@ -13,12 +18,14 @@ public class Board {
 		}
 	}
 	
+	public void addCardString(String card) {
+		cards.add(new Card(card));
+	}
+	
 	@Override
-	public String toString() {
-		String board = "[";
-		for (Card card : cards) {
-			board += " " + card;
-		}
-		return board + "]";
+	public String toString() {		
+		return cards.stream()
+					.map(Card::toString)
+				    .collect(Collectors.joining(" "));
 	}
 }
